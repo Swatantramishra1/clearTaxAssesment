15b10023de800565bbdf1b426a31bee94ec9de57
Swatantra <swatantramishra1@gmail.com>
2020-10-09 02:50:14 +0530

    added tooltip

--- src/app/data/data.ts
+++ src/app/data/data.ts
@@ -4,81 +4,6 @@ export const DATA = [
     totalAmount: 236.0,
     seats: [
       {
-        section: 'A',
-        sectionSeats: [
-          {
-            selected: true,
-            userSelected: false,
-            number: 1,
-          },
-          {
-            selected: false,
-            userSelected: false,
-            number: 2,
-          },
-          {
-            selected: false,
-            userSelected: false,
-            number: 3,
-          },
-          {
-            selected: false,
-            userSelected: false,
-            number: 4,
-          },
-          {
-            selected: true,
-            userSelected: false,
-            number: 5,
-          },
-          {
-            selected: false,
-            userSelected: false,
-            number: 6,
-          },
-          {
-            selected: false,
-            userSelected: false,
-            number: 7,
-          },
-          {
-            selected: false,
-            userSelected: false,
-            number: 8,
-          },
-          {
-            selected: false,
-            userSelected: false,
-            number: 9,
-          },
-          {
-            selected: true,
-            userSelected: false,
-            number: 10,
-          },
-          {
-            selected: true,
-            userSelected: false,
-            number: 11,
-          },
-          {
-            selected: true,
-            userSelected: false,
-            number: 12,
-          },
-          {
-            selected: false,
-            userSelected: false,
-            number: 13,
-          },
-          {
-            selected: false,
-            userSelected: false,
-            number: 14,
-          },
-        ],
-      },
-      {
         section: 'B',
         sectionSeats: [
           {
@@ -138,6 +63,81 @@ export const DATA = [
           },
           {
             selected: false,
+            userSelected: false,
+            number: 12,
+          },
+          {
+            selected: false,
+            userSelected: false,
+            number: 13,
+          },
+          {
+            selected: false,
+            userSelected: false,
+            number: 14,
+          },
+        ],
+      },
+      {
+        section: 'A',
+        sectionSeats: [
+          {
+            selected: true,
+            userSelected: false,
+            number: 1,
+          },
+          {
+            selected: false,
+            userSelected: false,
+            number: 2,
+          },
+          {
+            selected: false,
+            userSelected: false,
+            number: 3,
+          },
+          {
+            selected: false,
+            userSelected: false,
+            number: 4,
+          },
+          {
+            selected: true,
+            userSelected: false,
+            number: 5,
+          },
+          {
+            selected: false,
+            userSelected: false,
+            number: 6,
+          },
+          {
+            selected: false,
+            userSelected: false,
+            number: 7,
+          },
+          {
+            selected: false,
+            userSelected: false,
+            number: 8,
+          },
+          {
+            selected: false,
+            userSelected: false,
+            number: 9,
+          },
+          {
+            selected: true,
+            userSelected: false,
+            number: 10,
+          },
+          {
+            selected: true,
+            userSelected: false,
+            number: 11,
+          },
+          {
+            selected: true,
             userSelected: false,
             number: 12,
           },
